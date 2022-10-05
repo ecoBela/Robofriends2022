eff6a514b0ab06ff66e3c6a808744e39af091896
Bela Hamid <belahamid@gmail.com>
2022-10-06 00:36:24 +0100

    Delete files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-import Hello from "./Hello";
+import Card from "./Card";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Hello greeting={("Hello", "what's going on?")} />
+    <Card />
   </React.StrictMode>
 );
 
