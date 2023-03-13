f6ed627978409e1028e025f2461708f1eac61a27
Bela Hamid <belahamid@gmail.com>
2023-03-13 22:37:09 +0000

    Create store and pass to App

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import App from "./containers/App";
+import { searchRobots } from "./reducers";
+
+const store = createStore(searchRobots);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
